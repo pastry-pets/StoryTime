6747bd302d0e6c4f387073a1386c4361f8d9776b
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2026-01-08 11:56:40 -0600

    use dummy data

--- server/control.js
+++ server/control.js
@@ -6,27 +6,28 @@ let nextPromptTimer;
 
 // TODO: have way to select from vetted prompts
 function generateNewPrompt() {
-  return axios.get('https://random-word-api.herokuapp.com/word?number=5')
-    .then((response) => {
-      const words = response.data;
-      // save words to DB
-      return Badges.findOne({order: [['id', 'DESC']]})
-        .then((badge) => {
-          return Prompt.create({
-            matchWords: words.join(' '),
-            badgeId: badge.id
-          })
-            .then(() => {
-              return words;
-            })
-        })
-        .catch((error) => {
-          console.error('Failed to save words to database:',error);
-        })
-    })
-    .catch((error) => {
-      console.error('Failed to get words from API', error);
-    });
+  // return axios.get('https://random-word-api.herokuapp.com/word?number=5')
+  //   .then((response) => {
+  //     const words = response.data;
+  //     // save words to DB
+  //     return Badges.findOne({order: [['id', 'DESC']]})
+  //       .then((badge) => {
+  //         return Prompt.create({
+  //           matchWords: words.join(' '),
+  //           badgeId: badge.id
+  //         })
+  //           .then(() => {
+  //             return words;
+  //           })
+  //       })
+  //       .catch((error) => {
+  //         console.error('Failed to save words to database:',error);
+  //       })
+  //   })
+  //   .catch((error) => {
+  //     console.error('Failed to get words from API', error);
+  //   });
+  return new Promise((resolve, reject) => resolve(['this', 'is', 'a', 'test', 'array']));
 }
 
 module.exports = {
